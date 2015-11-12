0443ec2487494df59ab399bfa15af4cd0d08281e
Michael Obermüller <obermueller.michael@googlemail.com>
2015-11-12 19:30:38 +0100

    Add SongDurationValidator

---
+++ src/main/java/de/thi/mymusic/util/GuiUtil.java
@@ -0,0 +1,35 @@
+package de.thi.mymusic.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+
+public class GuiUtil {
+
+    /**
+     *
+     * Translation of FacesMessages
+     *
+     * @param context: FacesContext
+     * @param severity: Severity Level of FacesMessage
+     * @param msgKey: Key of property
+     * @param args: Optional arguements as message input
+     * @return: Translated FacesMessage
+     */
+    public static FacesMessage getFacesMessage(FacesContext context, FacesMessage.Severity severity,
+            String msgKey, Object... args) {
+        Locale loc = context.getViewRoot().getLocale();
+        ResourceBundle bundle = ResourceBundle.getBundle(context.getApplication().getMessageBundle(), loc);
+        String msg = bundle.getString(msgKey);
+
+        if (args != null) {
+            MessageFormat format = new MessageFormat(msg);
+            msg = format.format(args);
+        }
+
+        return new FacesMessage(severity, msg, null);
+    }
+}
---
+++ src/main/java/de/thi/mymusic/validation/SongDurationValidator.java
@@ -0,0 +1,33 @@
+package de.thi.mymusic.validation;
+
+import de.thi.mymusic.util.GuiUtil;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Michael Obermüller on 12.11.2015.
+ */
+
+@FacesValidator("songDurationValidator")
+public class SongDurationValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+        String duration = (String) value;
+
+        if (!Pattern.matches("^([0-5]?\\d):([0-5]?\\d)$", duration)) {
+            FacesMessage msg = GuiUtil.getFacesMessage(context, FacesMessage.SEVERITY_ERROR,
+                    "add.song.duration.formatError");
+
+            throw new ValidatorException(msg);
+        }
+    }
+
+}
