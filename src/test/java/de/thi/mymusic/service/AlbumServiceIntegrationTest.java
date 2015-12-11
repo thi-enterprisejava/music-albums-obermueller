a39ee29fd58b66547ba4ac3b14e36a6dd98f0ce5
Michael Obermüller <obermueller.michael@googlemail.com>
2015-12-11 13:50:31 +0100

    Add AlbumIntegrationTest file

---
+++ src/test/java/de/thi/mymusic/service/AlbumServiceIntegrationTest.java
@@ -0,0 +1,54 @@
+package de.thi.mymusic.service;
+
+import de.thi.mymusic.dao.CrudService;
+import de.thi.mymusic.domain.Album;
+import de.thi.mymusic.domain.BaseEntity;
+import de.thi.mymusic.domain.Interpret;
+import de.thi.mymusic.domain.Song;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.formatter.Formatters;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ejb.EJB;
+
+
+/**
+ * Created by Michael on 20.11.2015.
+ */
+
+@RunWith(Arquillian.class)
+public class AlbumServiceIntegrationTest {
+
+    @EJB
+    AlbumService albumService;
+
+    @Deployment(testable = true)
+    public static WebArchive createDeployment() {
+        WebArchive webarchive = ShrinkWrap.create(WebArchive.class, "test.war")
+                .addClass(BaseEntity.class)
+                .addClass(AlbumService.class)
+                .addClass(Album.class)
+                .addClass(Song.class)
+                .addClass(Interpret.class)
+                .addClass(CrudService.class)
+                /*.setWebXML("WEB-INF/web.xml")
+                .addAsWebInfResource("/src/main/webapp/WEB-INF/web.xml", "web.xml")
+                .addAsWebInfResource("WEB-INF/jboss-web.xml", "WEB-INF/jboss-web.xml")
+                .addAsWebInfResource("WEB-INF/faces-config.xml", "WEB-INF/faces-config.xml")*/
+                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+                ;
+        System.out.println(webarchive.toString(Formatters.VERBOSE));
+        return webarchive;
+    }
+
+
+    @Test
+    public void ThatAlbumCanBeAdded() {
+        //Album album = new Album();
+        //album.addSong(new Song(1, ""));
+    }
+}
